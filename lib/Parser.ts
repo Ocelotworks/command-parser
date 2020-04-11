24943d8d09008030a076380634b398e0101d5f1d
Peter Maguire <me@petermaguire.xyz>
2020-04-11 12:02:56 +0100

    parser module gl ci and command parsing

--- lib/Parser.ts
+++ lib/Parser.ts
@@ -11,9 +11,9 @@ export default class Parser {
     patterns: {[key: string]: any[]} = {};
 
     constructor(){
-        this.patterns["test"] = this.buildPattern(":arg1 :arg2 :arg3");
+        this.patterns["test"] = this.buildPattern(":arg1 :arg2 :arg3 [options:xyz,abc,pet] :optional? :optional2?");
 
-        this.parse("test ans1 ans2 ans3");
+        console.log(this.parse("test ans1 ans2 ans3 pet sbbb"));
     }
 
     buildPattern(pattern: string){
@@ -25,7 +25,7 @@ export default class Parser {
         let stack: any = {};
 
         function push(){
-            if(!stack.optional && output[output.length-1].optional && output[output.length-1].type !== "options"){
+            if(output.length > 1 && !stack.optional && output[output.length-1].optional && output[output.length-1].type !== "options"){
                 console.log(`Cannot have required parameter '${stack.name}' after optional non-options parameter '${output[output.length-1].name}'`)
                 return;
             }
@@ -39,7 +39,7 @@ export default class Parser {
             if(character === " ") {
                 if (state === STATE.SINGLE){
                     //End of single arg
-                    stack.name = this.getName(stack, pattern, tokenStart, i);
+                    stack.name = Parser.getName(stack, pattern, tokenStart, i);
                     push();
                 }else if(state !== STATE.ROOT){
                     console.log(`Malformed pattern char ${i} is '${character}' with state ${state}`);
@@ -47,7 +47,7 @@ export default class Parser {
                 }
             }else if(character === ":"){
                 if(state === STATE.OPTIONS_NAME){
-                    stack.name = this.getName(stack, pattern, tokenStart, i);
+                    stack.name = Parser.getName(stack, pattern, tokenStart, i);
                     state = STATE.OPTIONS;
                     stack.options = [];
                     stack.type = "options"
@@ -73,7 +73,7 @@ export default class Parser {
         }
 
         if(state === STATE.SINGLE){
-            stack.name = pattern.substring(tokenStart);
+            stack.name = Parser.getName(stack, pattern, tokenStart, pattern.length);
             push();
         }else if(state !== STATE.ROOT){
             console.log(`Pattern ended with open state ${state}`);
@@ -82,9 +82,10 @@ export default class Parser {
         return output;
     }
 
-    private getName(stack: any, pattern: string, tokenStart: number, i: number): string{
-        if(stack.optional)
+    private static getName(stack: any, pattern: string, tokenStart: number, i: number): string{
+        if(stack.optional) {
             return pattern.substring(tokenStart, i - 1);
+        }
         return pattern.substring(tokenStart, i);
     }
 
@@ -92,15 +93,44 @@ export default class Parser {
     parse(input: string): {id: string, data: any} {
         const endOfId = input.indexOf(" ")
         const id = input.substring(0, endOfId);
-        const args = input.substring(endOfId);
+        const args = input.substring(endOfId+1);
         const pattern = this.patterns[id];
 
+        let data = {};
+        let currentPosition = 0;
 
+        for(let i = 0; i < pattern.length; i++){
+            const argPattern = pattern[i];
+            const str = args.substring(currentPosition).trimLeft();
 
-        return {
-            id: "",
-            data: {}
+            if(str.length === 0){
+                if(!argPattern.optional)
+                    console.log(`Missing required argument ${argPattern.name}`);
+                data[argPattern.name] = null;
+                continue;
+            }
+
+            if(argPattern.type === "single"){
+                let index = str.indexOf(" ");
+                if(index == -1)
+                    index = str.length;
+                data[argPattern.name] = str.substring(0, index);
+                currentPosition += index+1;
+            }else if(argPattern.type === "options"){
+                let index = str.indexOf(" ");
+                if(index == -1)
+                    index = str.length;
+                let selectedOption = str.substring(0, index).toLowerCase();
+                data[argPattern.name] = argPattern.options.includes(selectedOption) ? selectedOption : null;
+
+                if(data[argPattern.name] === null && !argPattern.optional)
+                    console.log(`Invalid option for required argument ${argPattern.name}`);
+
+                currentPosition += index+1;
+            }
         }
+
+        return {id, data};
     }
 }
 
